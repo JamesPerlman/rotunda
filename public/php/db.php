a85fc1954a71553769bcd9a6056a2ecfc7c28893
JamesPerlman <jam.e.perl@gmail.com>
2014-05-14 12:24:19 -0700

    mysql

--- public/php/db.php
+++ public/php/db.php
@@ -1,9 +1,9 @@
 <?php
 	// connect to db
-	mysql_connect(getenv('OPENSHIFT_MYSQL_DB_HOST'), getenv('OPENSHIFT_MYSQL_DB_USERNAME'), getenv('OPENSHIFT_MYSQL_DB_PASSWORD'), "", getenv('OPENSHIFT_MYSQL_DB_PORT')) or die("Error: " . mysql_error());
+	/*mysql_connect(getenv('OPENSHIFT_MYSQL_DB_HOST'), getenv('OPENSHIFT_MYSQL_DB_USERNAME'), getenv('OPENSHIFT_MYSQL_DB_PASSWORD'), "", getenv('OPENSHIFT_MYSQL_DB_PORT')) or die("Error: " . mysql_error());
 mysql_select_db('james') or die("Error: " . mysql_error());
-/*
-	mysql_connect('127.0.0.1', 'adminrI7Qdbb', 'j4YB6Gm7Lvch');
+*/
+	mysql_connect('127.3.32.130', 'adminrI7Qdbb', 'j4YB6Gm7Lvch');
 	mysql_select_db('james') or die("Error: " . mysql_error());
-	*/
+	
 ?>
