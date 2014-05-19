f94787a5302231654b255587cb0f5b68b68606cf
JamesPerlman <jam.e.perl@gmail.com>
2014-05-18 21:24:48 -0700

    Version 0.9 - Getting close!  Fixed bugs, cleaned up code a little.  Nav is done except for loading content.

--- public/js/mainViewController.js
+++ public/js/mainViewController.js
@@ -1,17 +1,17 @@
 // Main Link Button Controller.Created by James Perlman on April 4th, 2014 at 12:06PM
 var lvc = (function(){
-	var navFolders = [{x:0,z:0,id:0}];
-	var center = {x:500, y:500};
-	var cam = {x:0,y:.8,z:2};
-	var curLevel = 1, lastLevel=1, curFolder=0, lastFolder=0, nav=[0];
-	var cscale = 600;
-	var cur = -1;
-	var enabled = true;
-	var fLen = 400;
-	var minAngle = Math.PI/6;
-	var model, parent;
-	var rings = [];
-	var tEffect = 0.25;
+	var cam = {x:0,y:.8,z:2},
+		center = {x:500, y:500},
+		cur = -1, curLevel = 1, curFolder=0,
+		enabled = true,
+		fLen = 400,
+		lastLevel=1, lastFolder=0,
+		minAngle = Math.PI/6,
+		model, navFolders = [{x:0,z:0,id:0}],
+		nav=[0],
+		parent,
+		rings = [],
+		tEffect = 0.25;
 	// this is very messy
 	function build(linkdata,element) {
 		if (linkdata && element) {
@@ -29,17 +29,13 @@ var lvc = (function(){
 					parent.appendChild(btn.div);
 					addEvents(btn.div,f[j]);
 					
-					if (i>curLevel)
-						btn.div.style.opacity = 0;
-						
+					if (i>curLevel) btn.div.style.opacity = 0;
 					var k = Math.min(1.5,i);
 					btn.x = k*rings[i][j].x;
 					btn.z = k*rings[i][j].z;
 				};
 			});
-			
 			setupDisplay();
-			
 		};
 	};
 	
@@ -60,12 +56,9 @@ var lvc = (function(){
 	}
 	function render3D() {
 		model.eachFolder(function(i,f) {
-			if(i<=curLevel) {
-				var obj,x,y,h,dy,k;
-				
+			if(i<=curLevel)
 				for(j=0;j<f.length;j++)
 					setPos3D(linkData.getLink(f[j]).view);
-			};
 		});
 		sortByDepth();
 	}
@@ -74,7 +67,6 @@ var lvc = (function(){
 		var mat = $("#rotator").css("transform").split(",");
 		return Math.atan2(mat[1],mat[3]);
 	}
-	
 	function setOffsets(){
 		model.eachFolder(function(i,f) {
 			if (i<=curLevel)
@@ -82,15 +74,14 @@ var lvc = (function(){
 					var obj = model.getLink(f[j]).view;
 					obj.setBase(obj.base.x,obj.base.y);
 				}
-			
 		});
 	};
 	function setPos3D(obj) {
 		dz = obj.z-cam.z;
 		k = fLen/dz;
-		// geometrically find x and y in 2d view
 		// resize object
 		obj.setSize(-100/dz);
+		// geometrically find x and y in 2d view
 		obj.setBase((cam.x-obj.x)*k+center.x,(cam.y-obj.y)*k+center.y);
 	}
 	// position elements
@@ -112,7 +103,6 @@ var lvc = (function(){
 		// remainder
 		// r = t - ~~(t/m); 
 		if (c==t) return 0;
-		
 		k=(t>c)?1:-1;
 		return m-k*(t-c)>k*(t-c) ? 1:-1;
 	}
@@ -123,80 +113,65 @@ var lvc = (function(){
 			view = model.getLink(i).view;
 			// add depth value of object at index i to array
 			z=view.z;
-			for (var j=0;j<arr.length;j++) {
+			for (var j=0;j<arr.length;j++)
 				// compare depth of object i to value in arr at index j
 				if (z<arr[j].z){
 					//alert(z+"<"+arr[j]+":"+(z<arr[j]));
 					p=j;// position sentinel (p=new position)
 					break;
 				}
-			}
+			
 			if(~p) //not found
 				arr.splice(p,0,{z:z,id:i});
 			else
 				arr.push({z:z,id:i});
-			
 			p=-1;
 		};
-		for (var i=0;i<arr.length;i++) {
+		for (var i=0;i<arr.length;i++)
 			model.getLink(arr[i].id).view.div.style.zIndex = i;
-		}
-		
 	}
 	// animation
 	function _changeFolder(id) {
-		
-		enabled=false;
-		cur = -1;
-		lastFolder = curFolder;
-		curFolder = id;
-		lastLevel = curLevel;
+		enabled=false; cur = -1; lastFolder = curFolder; curFolder = id; lastLevel = curLevel;
 		curLevel = model.getFolderIndex(curFolder);
-		// check which direction we are moving (into a folder or out of one)
 		TweenLite.ticker.addEventListener("tick", lvc.changeFolderTick);
 		TweenLite.to($("#rotator"), tEffect, {directionalRotation: "-90deg_short", onComplete:lvc.changeFolderDone});
 		
-		if (id > lastFolder) {
-			// moving into a new folder
+		if (id > lastFolder) { // moving into a new folder
 			nav.push(id);
 			k = 0;
-		} else {
-			// moving back to an old folder
+		} else { // moving back to an old folder
 			var i = nav.indexOf(lastFolder);
 			nav.splice(i, nav.length-i);
 			k = 1.5;
 		}
-		
-		
 		model.eachIn(lastFolder, function(i,obj) {
 			if (obj.id==id)return;
 			TweenLite.to(obj.view, tEffect, {x:rings[lastLevel][i].x*k,z:rings[lastLevel][i].z*k});
 			TweenLite.to(obj.view.div, tEffect, {opacity:0});
 		});
-			
 		model.eachIn(curFolder, function(i,obj) {
 			obj.view.div.style.visibility = "visible";
 			TweenLite.to(obj.view, tEffect, {x:rings[curLevel][i].x,z:rings[curLevel][i].z});
 			TweenLite.to(obj.view.div, tEffect, {opacity:1});
-		});// arrange folders
+		});
+		// arrange folders
 		var w=.27;
-		var l=-w*(nav.length-1)/2;
-		
-		for (var i=0; i<nav.length; i++) {
-			
+		var l=-w*(nav.length-1)/2;		
+		for (var i=0; i<nav.length; i++)
 			TweenLite.to(model.getLink(nav[i]).view, tEffect, {x: l+w*i, z:0});
-		}
 	}
 	function _changeFolderTick() {
 		setPos3D(model.getLink(0).view);
-		model.eachIn(curFolder, function(i,obj) { setPos3D(obj.view); });
-		model.eachIn(lastFolder, function(i,obj) { setPos3D(obj.view); });
+		model.eachIn(curFolder, function(i,obj) { setPos3D(obj.view); obj.view.draw(); });
+		model.eachIn(lastFolder, function(i,obj) { setPos3D(obj.view); obj.view.draw(); });
 		sortByDepth();
 	}
 	function _changeFolderDone() {
 		enabled = true;
 		_changeFolderTick();
 		model.eachIn(lastFolder, function(i,obj) { if (obj.id!=curFolder) obj.view.div.style.visibility="hidden"; });
+		model.eachIn(curFolder, function(i, obj) { setPos3D(obj.view); obj.view.draw(); });
 		TweenLite.ticker.removeEventListener("tick", lvc.changeFolderTick);
 		
 	}
@@ -204,12 +179,10 @@ var lvc = (function(){
 	function _itemMouseClick(id) {
 		if (!enabled) return;
 		var obj = model.getLink(id);
-		// folder
-		if( typeof obj.action == "number") {
+		if( typeof obj.action == "number") { // folder
 			if (curFolder!=id) _changeFolder(id);
 		} else
 			_rotateTo(id);
-		
 	}
 	function _itemMouseOut(id) {
 		TweenLite.to($('#btn'+id+" .ButtonTop .ButtonIcon"), tEffect,
@@ -218,20 +191,14 @@ var lvc = (function(){
 						{boxShadow:"0px -5px 15px #000"});
 	}
 	function _itemMouseOver(id) {
-		//alert(TweenLite);
 		TweenLite.to($('#btn'+id+" .ButtonTop .ButtonIcon"), tEffect,
 						{boxShadow:"0px 0px 15px #fff"});
 		TweenLite.to($('#btn'+id+" .ButtonReflection .IconReflection"), tEffect,
 						{boxShadow:"0px -5px 15px #555"});
 	}
-	function _itemMouseUp(id) {
-	}
 	// rotate on a timer
 	function _rotateTo(id) {
 		var d;
-		
-		//var theta = rot_angle()*180/Math.PI;
-		
 		if ((d=shortDist(rings[curLevel].length,cur,id))) {
 			enabled = false;
 			cur = id;
@@ -248,13 +215,7 @@ var lvc = (function(){
 		_rotateTick();
 		TweenLite.ticker.removeEventListener("tick", lvc.rotateTick);
 		enabled = true;
-		model.eachIn(curFolder, function(i,obj) {
-			obj.view.draw();
-			
-		});
-		//theta *= 180 / Math.PI;
-		
-						
+		model.eachIn(curFolder, function(i,obj) { setPos3D(obj.view); obj.view.draw(); });				
 	}
 	function _rotateTick() {
 		var theta = rot_angle();
@@ -272,7 +233,6 @@ var lvc = (function(){
 		sel:_itemMouseClick,
 		mOver:_itemMouseOver,
 		mOut:_itemMouseOut,
-		mUp:_itemMouseUp,
 		rotateDone:_rotateDone,
 		rotateTick:_rotateTick,
 		changeFolderDone:_changeFolderDone,
